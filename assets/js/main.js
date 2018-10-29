59ba84a7c60725ac9123956cc0cff210be122f22
xxsherryyy <sharebear31496@gmail.com>
2018-10-29 12:45:12 -0500

    portfolio update

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+// function myFunction() {
+// 	document.getElementById("github").innerHML 
+// 	document.getElementById("https://github.com/xxsherryyy").href
+// }
+jQuery(document).ready(function($) {
+    $('.slick.marquee').slick({
+      speed: 5000,
+      autoplay: true,
+      autoplaySpeed: 0,
+      centerMode: true,
+      cssEase: 'linear',
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      variableWidth: true,
+      infinite: true,
+      initialSlide: 1,
+      arrows: false,
+      buttons: false
+    });
+  });
